48cdf2108147e13750bfc36a8606918dc11eedae
uthayaa2005 <uthayaa.m2022cse@sece.ac.in>
2025-06-25 10:06:51 +0530

    error

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const boardRoutes = require('./routes/board');
-const upload = require('./middleware/upload');
 const fileUpload = require('express-fileupload')
 
 
@@ -30,7 +29,6 @@ app.get('/', (req, res) => {
 }); 
 app.use('/api/auth', authRoutes);
 app.use('/api/boards', boardRoutes);
-app.use('/api/upload', uploadRoute);
 app.use(fileUpload({ useTempFiles: true }));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
